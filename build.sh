3982f7e1145edb85af26ae1669766d3a99a1f2a4
Jose Renau <renau@ucsc.edu>
2018-05-22 18:37:49 -0700

    new docker setup

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 TAG_DATE=${TAG_DATE:-$(date +'%Y.%m')}
 
 docker build -f archlinux-masc/Dockerfile -t mascucsc/archlinux-masc:${TAG_DATE} ./archlinux-masc
+docker build -f ubuntu-masc/Dockerfile    -t mascucsc/ubuntu-masc:${TAG_DATE}    ./ubuntu-masc
 
--- publish.sh
+++ publish.sh
@@ -5,6 +5,8 @@ TAG_DATE=${TAG_DATE:-$(date +'%Y.%m')}
 docker login
 
 docker tag  mascucsc/archlinux-masc:${TAG_DATE} mascucsc/archlinux-masc:latest
-#docker push mascucsc/archlinux-masc:${TAG_DATE}
 docker push mascucsc/archlinux-masc:latest
+
+docker tag  mascucsc/ubuntu-masc:${TAG_DATE} mascucsc/ubuntu-masc:latest
+docker push mascucsc/ubuntu-masc:latest
 
