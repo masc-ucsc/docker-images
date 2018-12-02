2d38dfd87fc3831dd093bc2a3ba4fb6201155a52
Jose Renau <renau@ucsc.edu>
2018-12-01 22:31:35 -0800

    update in options

--- README.md
+++ README.md
@@ -7,11 +7,18 @@
  To run in interactive mode:
 
 ```
- docker run -it -e LOCAL_USER_ID=$(id -u $USER) -v $HOME:${HOME} mascucsc/archlinux-masc
+    docker run -it -e LOCAL_USER_ID=$(id -u $USER) -v $HOME:${HOME} mascucsc/archlinux-masc
 ```
 
 If you need to run gdb on the docker, you will also need to use the following flags:
 
 ```
-docker run --cap-add=SYS_PTRACE --security-opt seccomp=unconfined
+    docker run --cap-add=SYS_PTRACE --security-opt seccomp=unconfined
 ```
+
+To lunch a bazelcache (caches for a week, and clears old data)
+
+```
+    docker run --name bazelcache -d -p 8082:80 mascucsc/bazelcache-masc:latest
+```
+
--- publish.sh
+++ publish.sh
@@ -4,14 +4,14 @@ TAG_DATE=${TAG_DATE:-$(date +'%Y.%m')}
 
 docker login
 
-#docker tag  mascucsc/archlinux-masc:${TAG_DATE} mascucsc/archlinux-masc:latest
-#docker push mascucsc/archlinux-masc:latest
+docker tag  mascucsc/archlinux-masc:${TAG_DATE} mascucsc/archlinux-masc:latest
+docker push mascucsc/archlinux-masc:latest
 
-#docker tag  mascucsc/ubuntu-masc:${TAG_DATE} mascucsc/ubuntu-masc:latest
-#docker push mascucsc/ubuntu-masc:latest
+docker tag  mascucsc/ubuntu-masc:${TAG_DATE} mascucsc/ubuntu-masc:latest
+docker push mascucsc/ubuntu-masc:latest
 
 docker tag  mascucsc/alpine-masc:${TAG_DATE} mascucsc/alpine-masc:latest
 docker push mascucsc/alpine-masc:latest
 
-docker tag  mascucsc/bazelcache-masc:${TAG_DATE} mascucsc/bazelcache-masc:latest
-docker push mascucsc/bazelcache-masc:latest
+#docker tag  mascucsc/bazelcache-masc:${TAG_DATE} mascucsc/bazelcache-masc:latest
+#docker push mascucsc/bazelcache-masc:latest
