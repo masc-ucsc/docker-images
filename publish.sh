f11e939fed9e1d6e127672d8620d8f3e2ded39b8
Jose Renau <renau@ucsc.edu>
2019-10-18 13:16:00 -0700

    update kali image to handle more RISCV code/tests

--- publish.sh
+++ publish.sh
@@ -4,8 +4,11 @@ TAG_DATE=${TAG_DATE:-$(date +'%Y.%m')}
 
 docker login
 
-docker tag  mascucsc/archlinux-masc:${TAG_DATE} mascucsc/archlinux-masc:latest
-docker push mascucsc/archlinux-masc:latest
+#docker tag  mascucsc/archlinux-masc:${TAG_DATE} mascucsc/archlinux-masc:latest
+#docker push mascucsc/archlinux-masc:latest
+
+docker tag  mascucsc/kaliriscv-masc:${TAG_DATE} mascucsc/kaliriscv-masc:latest
+docker push mascucsc/kaliriscv-masc:latest
 
 #docker tag  mascucsc/ubuntu-masc:${TAG_DATE} mascucsc/ubuntu-masc:latest
 #docker push mascucsc/ubuntu-masc:latest
