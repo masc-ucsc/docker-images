713ced274116efc160e088fb1a61cc33058d1e99
Jose Renau <renau@ucsc.edu>
2025-08-12 13:10:54 -0700

    update confs

--- publish.sh
+++ publish.sh
@@ -4,18 +4,23 @@ TAG_DATE=${TAG_DATE:-$(date +'%Y.%m')}
 
 docker login
 
-docker tag mascucsc/archlinux-masc:${TAG_DATE} mascucsc/archlinux-masc:latest
-docker push mascucsc/archlinux-masc:latest
+#docker tag mascucsc/archlinux-masc:${TAG_DATE} mascucsc/archlinux-masc:latest
+#docker push mascucsc/archlinux-masc:latest
 
-docker tag mascucsc/alpine-masc:${TAG_DATE} mascucsc/alpine-masc:latest
-docker push mascucsc/alpine-masc:latest
+#docker tag mascucsc/alpine-masc:${TAG_DATE} mascucsc/alpine-masc:latest
+#docker push mascucsc/alpine-masc:latest
 
 docker tag mascucsc/hagent-builder:${TAG_DATE} mascucsc/hagent-builder:latest
+docker push mascucsc/hagent-builder:${TAG_DATE}
 docker push mascucsc/hagent-builder:latest
 
 docker tag mascucsc/hagent-xiangshan:${TAG_DATE} mascucsc/hagent-xiangshan:latest
 docker push mascucsc/hagent-xiangshan:${TAG_DATE}
 docker push mascucsc/hagent-xiangshan:latest
+
+docker tag mascucsc/hagent-cva6:${TAG_DATE} mascucsc/hagent-cva6:latest
+docker push mascucsc/hagent-cva6:${TAG_DATE}
+docker push mascucsc/hagent-cva6:latest
 
 docker tag mascucsc/hagent-simplechisel:${TAG_DATE} mascucsc/hagent-simplechisel:latest
 docker push mascucsc/hagent-simplechisel:${TAG_DATE}
